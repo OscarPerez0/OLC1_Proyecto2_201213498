c25a50c16dab6f8c207c67ba424c61bed47ee970
Oscar Perez <oscar.6293@gmail.com>
2020-11-04 15:03:46 -0600

    python1

---
+++ app-express-py/python-service/Entradas/test1.java
@@ -0,0 +1,22 @@
+public class test{
+
+	int numero = 5+6*(8/4) + (a*b /36) -50;
+
+
+	public void imprimir() {
+		for(int i = 0; i < 10+1; i++) {
+			System.out.println("esto es una cadena");
+		}
+	}
+
+	public int suma(int a, int b) {
+		if (a == b && b >= c) {
+			return "cadena";
+		}
+		return a + b;
+	}
+		
+}
+
+
+
